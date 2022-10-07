0f7efa6ab346d282a734a7df34f316867b014ecf
Igor Arecippo <iarecippo@gmail.com>
2022-10-07 18:47:38 -0300

    requisito 1

---
+++ src/redux/reducers/playerReducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {
+  player: {
+    name: '',
+    assertions: 0,
+    score: 0,
+    gravatarEmail: '',
+  },
+};
+
+function playerReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default playerReducer;
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,9 @@
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer, composeWithDevTools(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 if (window.Cypress) {
   window.store = store;
