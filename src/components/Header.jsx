88674081db1a9c183bd44bbfe2da046408370ed9
Rafael Rodrigues Bizachi <rafbizachi@gmail.com>
2022-10-10 18:23:07 -0300

    finalizando req 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,36 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { MD5 } from 'crypto-js';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email, name, score } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <h1>teste</h1>
+        <h1 data-testid="header-player-name">{ name }</h1>
+        <h2 data-testid="header-score">{ score }</h2>
+        <img
+          src={ `https://www.gravatar.com/avatar/${MD5(email)}` }
+          data-testid="header-profile-picture"
+          alt="Imagem do perfil"
+        />
+      </header>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.any,
+  name: PropTypes.any,
+  score: PropTypes.any,
+}.isRequired;
+
+mapStateToProps = (state) => ({
+  gravatarEmail: state.player.email,
+  score: state.player.score,
+  name: state.player.name,
+});
+
+export default connect()(Header);
