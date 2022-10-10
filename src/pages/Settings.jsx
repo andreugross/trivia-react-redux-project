1f2183456606bfaaa64f4fd844e011c8108971ae
André Gross <andgross@gmail.com>
2022-10-10 11:05:05 -0300

    iniciando req 3

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <div
+        data-testid="settings-title"
+      >
+        <h1>Configuraçoes</h1>
+      </div>
+    );
+  }
+}
