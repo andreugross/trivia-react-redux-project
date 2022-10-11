39c4e154bfd530a0edc6dc946243c8cd6a72831f
Rafael Rodrigues Bizachi <rafbizachi@gmail.com>
2022-10-11 17:38:49 -0300

    requisito 4 90% de obertura

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,23 +2,44 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import App from '../App';
 
-beforeEach(() => {
-  renderWithRouterAndRedux(<Login />);
-});
 
 describe('Verifica página inicial de logim', () => {
   it('Verifica se possui o texto Login', () => {
+    renderWithRouterAndRedux(<App />);
     const textLogin = screen.getByRole('heading', {
       name: /login/i,
     });
-    const btnSettings = screen.getByRole('button', {  name: /settings/i});
+   const btnSettings = screen.getByRole('button', {  name: /settings/i});
     
     expect(btnSettings).toBeInTheDocument();
     expect(textLogin).toBeInTheDocument();
   });
 
+  it('verifica se o botão play vai para a pagina /game', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const buttonPlay = screen.getByRole('button', { name: /play/i });
+    const inputName = screen.getByTestId('input-player-name');
+    const inputEmail = screen.getByTestId('input-gravatar-email');
+
+    userEvent.type(inputName, 'teste');
+    userEvent.type(inputEmail, 'pessoa@gmail.com');
+    userEvent.click(buttonPlay);
+
+    expect(history.location.pathname).toBe('/game');
+  });
+  it('verifica se o botão settings vai para a pagina /settings', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const btnSettings = screen.getByRole('button', {  name: /settings/i});
+    
+    userEvent.click(btnSettings);
+
+    expect(history.location.pathname).toBe('/settings');
+  });
+
   it('Verifica se é possível escrever nos inputs de email e password', () => {
+    renderWithRouterAndRedux(<Login />);
     const inputName = screen.getByTestId('input-player-name');
     const inputEmail = screen.getByTestId('input-gravatar-email');
     const buttonPlay = screen.getByRole('button', { name: /play/i });
