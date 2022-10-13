1c94eda207d4fe42d1c21ede4e27abf602db09f5
André Gross <andgross@gmail.com>
2022-10-13 10:41:17 -0300

    incia req 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
     return (
-      <div>Feedback</div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
